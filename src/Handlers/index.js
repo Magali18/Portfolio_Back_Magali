0804cb862a5ad481e1c700f61b2247273e452e08
Magali18 <pereyrahmagali@gmail.com>
2023-09-09 14:59:33 -0300

    delete/idVisit

--- src/Handlers/index.js
+++ src/Handlers/index.js
@@ -1,8 +1,7 @@
 const postVisitHandler = require ("./postVisit");
 const getAllVisit = require( "./getAllVisit")
-const getVisitIdHandler = require('./getVisitIdHandler')
+
 module.exports={
     postVisitHandler,
     getAllVisit,
-    getVisitIdHandler 
 }
