01cf55108c1bb7eb633d66bdc1ee8647a2b48262
Magali18 <pereyrahmagali@gmail.com>
2023-09-26 18:06:08 -0300

    add/wpp/text

--- src/Controller/postWppController.js
+++ src/Controller/postWppController.js
@@ -1,26 +1,33 @@
 const messageWpp = require("../db");
 const axios = require("axios");
+require('dotenv').config();
+
+
+const {
+  TOKEN_WPP
+} = process.env;
 
 const header = {
   headers: {
-    Authorization: `BearerEAAE8DpOkrsQBOxKD6u5ZCjGZA4d5N4Oov8vTb2V025R0i0vJ6ZBW19UIZARLLlZBQSsbS5tN91nA55reFNeWVCsW110es8BzyfmRUO5SLXCOR3h7L99bPlm9lCWpASrbK00YWZCUsyTAecSjh9zzOWc3K9YjjxW30BkQ0ysOIwJCHnLOZC2MIKZB6KxZB`,
-    Accept: 'application/json, text/plain, */*',
-    'Content-Type': 'application/json',
+    Authorization: `Bearer ${TOKEN_WPP}`
+
   },
 };
+const tel = 372819103
+const saludo ='Hola ciomo estas' + tel
 
 const postWppController = async (name, number, message) => {
   const data = {
-    "messaging_product": "whatsapp",
-    "to": "543883371406",
-    "type": "template",
-    "template": {
-      "name": "hello_world",
-      "language": {
-        "code": "en_US"
-      }
+    messaging_product: "whatsapp",
+    to: "543883371406",
+    type: "text", 
+    text: { 
+      preview_url: false, 
+      body:saludo
+       
     }
   }
+  console.log('------------------>')
 
   try {
     const apiResponse = await axios.post(
