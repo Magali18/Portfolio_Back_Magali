97595387a333d9c21379f952ba80870d4cf422fb
Magali18 <pereyrahmagali@gmail.com>
2023-09-29 17:12:41 -0300

    delet/controller/handler/rellacionado con la DB

--- src/Handlers/index.js
+++ src/Handlers/index.js
@@ -1,9 +1,5 @@
-const postVisitHandler = require ("./postVisit");
-const getAllVisit = require( "./getAllVisit")
 const wppPost =require('./wppPost')
 
 module.exports={
-    postVisitHandler,
-    getAllVisit,
     wppPost
 }
