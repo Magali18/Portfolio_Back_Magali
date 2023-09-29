0f9a0a70cec337120eaad68b273109ddc872cba5
Magali18 <pereyrahmagali@gmail.com>
2023-09-29 17:08:15 -0300

    delete/conection/DB

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const server = require('./src/app') 
-const { conn } = require('./src/db.js');
 const port = process.env.PORT
 
-conn.sync({ force: false })
-  .then(() => {
+
+ 
     server.listen(port, () => {
       console.log(`%s listening at ${port}`);
     });
-  })
-  .catch((error) => {
-    console.error('Error syncing database:', error);
-  });
+  
